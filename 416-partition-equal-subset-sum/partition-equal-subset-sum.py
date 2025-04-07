1d2676107f873651a62a48ed36b5ba01f5ff4f18
S4srihari <85178734+S4srihari@users.noreply.github.com>
2025-04-07 00:39:30 -0400

    Time: 217 ms (92.35%) | Memory: 19.3 MB (57.75%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -3,10 +3,22 @@ class Solution:
         s = sum(nums)
         if s%2 == 1: return False
         half = s//2
-        dp = [True] + [False]*half
+        dp = set([0])
+        for num in nums:
+            temp = set()
+            for p in dp:
+                if num + p == half:
+                    return True
+                temp.add(num+p)
+                temp.add(p)
+            dp = temp
+        return False
+
+
+        """dp = [True] + [False]*half
         for num in nums:
             for i in range(half, num-1,-1):
                 dp[i] = dp[i] or dp[i-num]
             if dp[-1] :return True
 
-        return False 
+        return False """
