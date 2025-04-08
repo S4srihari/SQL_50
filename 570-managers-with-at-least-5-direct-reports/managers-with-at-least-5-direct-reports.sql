0b1fcf9c031e38eaa0747ffae10435dc793bac28
S4srihari <85178734+S4srihari@users.noreply.github.com>
2025-04-08 11:37:24 -0400

    Time: 381 ms (77.88%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
 SELECT name FROM Employee WHERE id in (
     SELECT managerId FROM Employee 
-    GROUP BY managerId HAVING Count(id) > 4 
+    GROUP BY managerId HAVING Count(managerId) > 4 
 );
