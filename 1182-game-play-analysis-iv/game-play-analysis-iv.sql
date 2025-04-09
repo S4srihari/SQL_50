ce41028cd940f7f7860d07779ac61341b911682c
S4srihari <85178734+S4srihari@users.noreply.github.com>
2025-04-09 15:24:56 -0400

    Time: 572 ms (84.77%) | Memory: 0B (100.00%) - LeetSync

--- 1182-game-play-analysis-iv/game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -1,5 +1,13 @@
 # Write your MySQL query statement below
-SELECT ROUND(COUNT(A.player_id)/COUNT(F.player_id),2) AS fraction FROM Activity A 
-RIGHT JOIN (SELECT player_id, min(event_date) AS F_date
-FROM Activity GROUP BY player_id)F ON
-A.player_id = F.player_id AND DATEDIFF(A.event_date,F.F_date) = 1 ;
+
+#SELECT ROUND(COUNT(A.player_id)/COUNT(F.player_id),2) AS fraction FROM
+#(SELECT player_id, min(event_date) AS F_date
+#FROM Activity GROUP BY player_id)F LEFT JOIN Activity A ON
+#A.player_id = F.player_id AND DATEDIFF(A.event_date,F.F_date) = 1 ;
+
+SELECT ROUND( COUNT(DISTINCT player_id) / (SELECT COUNT( DISTINCT player_id) FROM Activity) ,2) AS fraction
+FROM Activity WHERE 
+(player_id, event_date) in (
+    SELECT player_id, min(event_date) + INTERVAL 1 DAY AS event_date FROM Activity 
+    GROUP BY player_id
+);
